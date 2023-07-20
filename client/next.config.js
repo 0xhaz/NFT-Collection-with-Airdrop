c8e54a52d57d96eefe2e49e6405be6d315c91afe
0xhaz <28639279+0xhaz@users.noreply.github.com>
2023-07-20 23:07:01 +0800

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  externals: [
-    {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
       bufferutil: "commonjs bufferutil",
-    },
-  ],
+    });
+    return config;
+  },
   images: {
     domains: ["*"],
     remotePatterns: [
